a06a56b64762cb6992e7c77cd1b03d6b7fe407fa
Ayedimohamed <52761890+Ayedimohamed@users.noreply.github.com>
2025-09-29 15:27:47 +0100

    Description des modifications

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
   apiUrl: 'https://votre-api.domain.com',
-  CLIENT_SERVER_URI : 'https://api.pawty.tn/',
+  CLIENT_SERVER_URI: 'http://localhost:3001/',
+  // CLIENT_SERVER_URI : 'https://api.pawty.tn/',
   SOCKETS_SERVER_URI: 'https://socket.pawty.tn/',
   CRYPT_SECRET: 'qskO2CFWHNJd1KN1eWP-9QWl6wPqPjvu:)SN._I@n+)5PSMViaE!pP)teWKO)-DQPHl_-YZz)?iJEg2XIOaM_nG!lpg)jB7-MHv=*2Tr)zqG1y.b3#rDoM+vzYdQK3k6',
   CSRF_SECRET :"5BUISZSLSPdy5p0yleIpUsZhMpb715tLtfxbmSnOqtlWJCoOGvcYTcLZnOJnShwmG9IliGq8MfVEfc7UfGqYkZgj0D0CHhgMKkpsVXA8oo7+pmKQNGFrp8EBojvlP/Lw",
